e26a4547c76d62ed87ea3a045868586f8dc53a14
Hallie Hughes <hnhughes@wisc.edu>
2026-09-17 20:28:19 -0500

    task6.sh 5th update

--- HW01/task6.cpp
+++ HW01/task6.cpp
@@ -5,12 +5,18 @@
 int main(int argc, char *argv[]) {
     int N = std::atoi(argv[1]);
     for (int i=0; i<=N; i++){
-        std::printf("%d ",i);
+        std::printf("%d",i);
+        if (i<N) {
+            std::printf(" ");
+        }
     }
     std::printf("\n");
 
     for (int i=N; i>=0; i--){
-        std::cout << i << " ";
+        std::cout << i;
+        if (i>0){
+            std::cout << " ";
+        }
     }
     std::cout << "\n";
 
